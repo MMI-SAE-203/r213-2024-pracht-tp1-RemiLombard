a9cbc4d004a42a809cc1d3bfbe74b7df0b27f426
Rémi-Lombard <remi.lombard@edu.univ-fcomte.fr>
2024-04-11 17:23:10 +0200

    boucle

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const maVar = ref(false)
-const menuIsOpen = ref(false)
 </script>
 
 
 <template>
-  <h1 v-show="maVar" class="text-2xl">Bonjour monde !</h1>
+  <h1 class="text-2xl">Bonjour monde !</h1>
 </template>
